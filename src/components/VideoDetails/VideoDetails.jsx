d02db2b0130baa1b6bbcb61f41056d84c36cfcd6
Ash Jones <ashjones0515@gmail.com>
2022-11-03 16:33:02 -0700

    tablet styled for video details

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -32,45 +32,47 @@ function VideoDetails(props) {
 
 	return (
 		<section className='vid-details'>
-			<div className='vid-details__title'>{title}</div>
-			<div className='vid-details__info-wrapper'>
-				<div className='vid-details__info-left'>
-					<div className='vid-details__info-left--by'>
-						<p className='vid-details__info-left--by-info'>
-							{channel}
-						</p>
+			<div className='vid-details__wrapper'>
+				<div className='vid-details__title'>{title}</div>
+				<div className='vid-details__info-wrapper'>
+					<div className='vid-details__info-left'>
+						<div className='vid-details__info-left--by'>
+							<p className='vid-details__info-left--by-info'>
+								{channel}
+							</p>
+						</div>
+						<div className='vid-details__info-left--date'>
+							<p className='vid-details__info-left--date-info'>
+								{videoRelativeTime(timestamp)}
+							</p>
+						</div>
 					</div>
-					<div className='vid-details__info-left--date'>
-						<p className='vid-details__info-left--date-info'>
-							{videoRelativeTime(timestamp)}
-						</p>
+					<div className='vid-details__info-right'>
+						<div className='vid-details__info-right--views'>
+							<img
+								className='vid-details__info-right--views-icon'
+								src={viewsIcon}
+								alt='views icon'
+							/>
+							<p className='vid-details__info-right--views-info'>
+								{views}
+							</p>
+						</div>
+						<div className='vid-details__info-right--likes'>
+							<img
+								className='vid-details__info-right--likes-icon'
+								src={likesIcon}
+								alt='like icon'
+							/>
+							<p className='vid-details__info-right--likes-info'>
+								{likes}
+							</p>
+						</div>
 					</div>
 				</div>
-				<div className='vid-details__info-right'>
-					<div className='vid-details__info-right--views'>
-						<img
-							className='vid-details__info-right--views-icon'
-							src={viewsIcon}
-							alt='views icon'
-						/>
-						<p className='vid-details__info-right--views-info'>
-							{views}
-						</p>
-					</div>
-					<div className='vid-details__info-right--likes'>
-						<img
-							className='vid-details__info-right--likes-icon'
-							src={likesIcon}
-							alt='like icon'
-						/>
-						<p className='vid-details__info-right--likes-info'>
-							{likes}
-						</p>
-					</div>
+				<div className='vid-details__description-section'>
+					<p className='vid-details__description'>{description}</p>
 				</div>
-			</div>
-			<div className='vid-details__description'>
-				<p>{description}</p>
 			</div>
 		</section>
 	);
