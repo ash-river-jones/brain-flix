d90c2dcf22ac63d7db528d53eec609cf70d8e5f2
Ash Jones <ashjones0515@gmail.com>
2022-11-07 17:14:38 -0800

    styling done for mobile, tablet and desktop --- upload page

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -8,49 +8,54 @@ export default function Upload() {
 			</div>
 			<div className='upload__wrapper'>
 				<form className='upload__form' action='submit'>
-					<div className='upload__video-section'>
-						<p className='upload__label'>VIDEO THUMBNAIL</p>
-						<img
-							className='upload__thumbnail'
-							src={uploadThumbnail}
-							alt='Upload Video Thumbnail'
-						/>
-					</div>
-					<div className='upload__details'>
-                        <div className="upload__details-title">
-                            <label className='upload__label' htmlFor='title'>
-                                TITLE YOUR VIDEO
-                            </label>
-                            <input
-                                className='upload__title'
-                                name='title'
-                                id='title'
-                                placeholder='Add a title to your video'
-                                type='text'
+                    <div className="upload__form-wrapper">
+                        <div className='upload__video-section'>
+                            <p className='upload__label'>VIDEO THUMBNAIL</p>
+                            <img
+                                className='upload__thumbnail'
+                                src={uploadThumbnail}
+                                alt='Upload Video Thumbnail'
                             />
                         </div>
-                        <div className="upload__details-desc">
-                            <label className='upload__label' htmlFor='desc'>
-                                ADD A VIDEO DESCRIPTION
-                            </label>
-                            <textarea
-                                className='upload__desc'
-                                name='desc'
-                                id='desc'
-                                placeholder='Add a description to your video'
-                                cols="30" 
-                                rows="10"
-                            ></textarea>
+                        <div className='upload__details-wrapper'>
+                            <div className='upload__details'>
+                                <label className='upload__label' htmlFor='title'>
+                                    TITLE YOUR VIDEO
+                                </label>
+                                <input
+                                    className='upload__title'
+                                    name='title'
+                                    id='title'
+                                    placeholder='Add a title to your video'
+                                    type='text'
+                                />
+                            </div>
+                            <div className='upload__details'>
+                                <label className='upload__label' htmlFor='desc'>
+                                    ADD A VIDEO DESCRIPTION
+                                </label>
+                                <textarea
+                                    className='upload__desc'
+                                    name='desc'
+                                    id='desc'
+                                    placeholder='Add a description to your video'
+                                    cols='30'
+                                    rows='5'
+                                ></textarea>
+                            </div>
                         </div>
-					</div>
-					<div className='upload__btn-section'>
-						<button className='upload__btn-cancel' type='reset'>
-							CANCEL
-						</button>
-						<button className='upload__btn-publish' type='submit'>
-							PUBLISH
-						</button>
-					</div>
+                    </div>
+                    <div className='upload__btn-section'>
+                        <div className='upload__btn-wrapper'>
+                            <button className='upload__btn-publish' type='submit'>
+                                PUBLISH
+                            </button>
+                            <button className='upload__btn-cancel' type='reset'>
+                                CANCEL
+                            </button>
+                        </div>
+                    </div>
+                    
 				</form>
 			</div>
 		</section>
